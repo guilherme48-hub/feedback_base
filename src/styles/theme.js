2b35af64ebcf6f649a9cec9a5ea9a60e0646f093
Guilherme Santos Gomes <guistsgomes07@gmail.com>
2024-06-09 14:49:10 +0000

    intalaçao

---
+++ src/styles/theme.js
@@ -0,0 +1,7 @@
+ export const theme = {
+    HomeBackgroud: "#131518",
+    cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)",
+    textColor: "#969FAD",
+    submitbuttoncolor: "#262E38",
+    feedbackButtonText:"#7C8798",
+}
