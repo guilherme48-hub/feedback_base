cef35619204640fb7b6d33228b342ce3e557db17
Guilherme Santos Gomes <guistsgomes07@gmail.com>
2024-06-09 17:47:12 +0000

    style:acabou

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import { ButtonContainer, CardContainer, IconContainer } from "./Home.Styles";
+import { ButtonContainer, CardContainer, IconContainer, ResulContainer } from "./Home.Styles";
 import iconStarImg from "../assets/icon-star.svg";
+import thankYouImg from "../assets/illustration-thank-you.svg";
 
 export function Home() {
+  let aparecerResutado = true
+
   return (
-    <CardContainer>
+    aparecerResutado === false ? (
+      <CardContainer>
       <IconContainer>
         <img src={iconStarImg} alt="icone de estrela" />
       </IconContainer>
@@ -13,14 +17,27 @@ export function Home() {
       <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
 
       <ButtonContainer>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
       </ButtonContainer>
 
       <button>enviar</button>
     </CardContainer>
-  );
+    ) : (
+      <CardContainer>
+        <img src={thankYouImg} alt="imagem so agradece" />
+
+        <ResulContainer>
+          <p>você selecionou 4 de 5</p>
+        </ResulContainer>
+
+        <h1>obrigado!</h1>
+
+        <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato!</p>
+      </CardContainer>
+    )
+  )
 }
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,6 +2,16 @@
     homeBackground: "#131518",
     cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)",
     textColor: "#969FAD",
-    submitbuttoncolor: "#262E38",
+    submitbuttoncolor: "#FC7614",
+    white:"#FFFFFF",
+    feedbackButtoncolor:"#262E38",
     feedbackButtonText:"#7C8798",
+   
+
+
+
+
+
+
+  
 }
