917d92cc50eb5b1ed724554423ff0c3514f651dd
Guilherme Santos Gomes <guistsgomes07@gmail.com>
2024-06-09 15:05:35 +0000

    style: global

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { ThemeProvider } from "styled-components"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
-import{Home} from "./pages/Home"
-import {theme} from "./styles/theme.js"
+import { Home } from "./pages/Home";
+import { theme } from "./styles/theme";
+import { GlobalStyle } from "./styles/global";
 
-const router = createBrowserRouter ([
+const router = createBrowserRouter([
     {
         path: "/",
-        Element: <Home />
+        element: <Home />
     }
-])
+]);
 
-
-export  function App() {
-  return (
-   <ThemeProviderTheme={theme}>
-
-    <RouterProvider router={router} />
-   </ThemeProvider>
-  )
+export function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <RouterProvider router={router} />
+            <GlobalStyle />
+        </ThemeProvider>
+    );
 }
